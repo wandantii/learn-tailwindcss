e00b25a24ea65bbe7c509f521355b8a4d027b0ce
Wanda Berlian Mardianti <69835730+wandantii@users.noreply.github.com>
2023-10-12 14:32:14 +0700

    feat: end

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['./public/index.html'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        13: '3.25rem',
+      },
+      fontFamily: {
+        inter: ['inter'],
+      },
+      colors: {
+        wanda: '#32a832',
+        berlian: '#6737ad',
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'goyang': 'goyang 1s ease-in-out infinite',
+      },
+      keyframes: {
+        goyang: {
+          '0%, 100%': {transform:'rotate (-3deg)'},
+          '50%': {transform:'rotate(3deg)'},
+        },
+      },
+    },
   },
   plugins: [],
 }
